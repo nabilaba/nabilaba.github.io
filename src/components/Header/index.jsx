115daf30338251a1e36e0ce6b98c8e4df5489f17
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-24 00:03:40 +0700

    update header like vscode header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,44 @@
-import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import { Flex, HStack, Icon, Text, useColorModeValue } from "@chakra-ui/react";
+import { FaJs, FaReact, FaSass } from "react-icons/fa";
 
 const HeaderContent = () => (
   <Flex
     as="header"
     align="center"
     w="full"
-    px="4"
+    px="1"
     borderColor={useColorModeValue("inherit", "gray.700")}
     bg={useColorModeValue("white", "gray.900")}
-    h="4vh"
     position="sticky"
     top="0"
   >
-    <Text align="center">nabilaba.github.io</Text>
+    <HStack spacing={1} h="full">
+      <HeaderItem icon={FaJs} label="skills.js" color="yellow.500" />
+      <HeaderItem icon={FaReact} label="projects.jsx" color="blue.500" />
+      <HeaderItem icon={FaSass} label="educations.sass" color="pink.500" />
+    </HStack>
   </Flex>
+);
+
+const HeaderItem = ({ icon, label, color }) => (
+  <HStack
+    color={useColorModeValue("gray.500", "gray.400")}
+    spacing={1}
+    align="center"
+    role="group"
+    h="full"
+    fontSize={".75rem"}
+    cursor="pointer"
+    w="max-content"
+    borderRightWidth={2}
+    borderRightColor={useColorModeValue("gray.50", "gray.800")}
+    _hover={{ color: useColorModeValue("gray.700", "white") }}
+    py="2"
+    px="3"
+  >
+    {icon && <Icon boxSize="5" as={icon} color={color} />}
+    {label && <Text>{label}</Text>}
+  </HStack>
 );
 
 export default HeaderContent;
