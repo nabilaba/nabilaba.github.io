7aca1ebadd93e4d762c4c51b5bdbfe54139a5e94
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-23 21:12:57 +0700

    changing footer icon to react-icons library instead of codicons

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,6 +6,14 @@ import {
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/react";
+import {
+  VscSourceControl,
+  VscCloudUpload,
+  VscError,
+  VscWarning,
+  VscSmiley,
+} from "react-icons/vsc";
 
 const FooterContent = () => (
   <Flex
@@ -21,20 +29,15 @@ const FooterContent = () => (
     px={2}
     justifyContent="space-between"
   >
-    <HStack spacing={1} h="full">
-      <FooterItem icon="codicon codicon-git-branch" label="main*" />
-      <FooterItem icon="codicon codicon-cloud-upload" />
-      <FooterItem
-        icon="codicon codicon-error"
-        label="0"
-        icon2="codicon codicon-warning"
-        label2="0"
-      />
+    <HStack h="full">
+      <FooterItem icon={VscSourceControl} label="main*" />
+      <FooterItem icon={VscCloudUpload} />
+      <FooterItem icon={VscError} label="0" icon2={VscWarning} label2="0" />
       <FooterItem label="Git Graph" />
     </HStack>
-    <HStack spacing={1} h="full">
+    <HStack h="full">
       <FooterItem label="Javascript" label2="React" />
-      <FooterItem icon="codicon codicon-smiley" />
+      <FooterItem icon={VscSmiley} />
     </HStack>
   </Flex>
 );
@@ -46,17 +49,15 @@ const FooterItem = ({ icon, icon2, label, label2 }) => (
     align="center"
     role="group"
     h="full"
-    fontSize={".55rem"}
+    fontSize={".75rem"}
     cursor="pointer"
     w="max-content"
     _hover={{ bg: useColorModeValue("gray.100", "gray.800") }}
     p={1}
   >
-    {icon && <Text as="span" className={icon} style={{ fontSize: ".75rem" }} />}
+    {icon && <Icon boxSize="4" as={icon} />}
     {label && <Text>{label}</Text>}
-    {icon2 && (
-      <Text as="span" className={icon2} style={{ fontSize: ".75rem" }} />
-    )}
+    {icon2 && <Icon boxSize="4" as={icon2} />}
     {label2 && <Text>{label2}</Text>}
   </HStack>
 );
