2d3a56e3219a890459ddf5bfa2fe2ef551d1255e
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-24 02:05:11 +0700

    added navigate onclick in logo

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -15,114 +15,126 @@ import { FaTelegramPlane, FaLinkedin } from "react-icons/fa";
 import { GoMarkGithub } from "react-icons/go";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { TiSocialInstagram } from "react-icons/ti";
+import { useNavigate } from "react-router-dom";
 
-const SidebarContent = (props) => (
-  <Box
-    as="nav"
-    pos="fixed"
-    top="0"
-    left="0"
-    pb="25px"
-    zIndex="1"
-    h="full"
-    overflowX="hidden"
-    overflowY="auto"
-    bg={useColorModeValue("gray.50", "gray.700")}
-  >
-    <VStack h="full" w="full" alignItems="flex-start" justify="space-between">
-      <Box w="full">
-        <Flex px="2" py="5" align="center">
+const SidebarContent = (props) => {
+  const navigate = useNavigate();
+  const NavItem = (props) => {
+    const color = useColorModeValue("gray.600", "gray.300");
+
+    const { icon, url } = props;
+    return (
+      <Flex
+        as={Link}
+        href={url}
+        target="_blank"
+        align="center"
+        px="2"
+        py="4"
+        cursor="pointer"
+        role="group"
+        fontWeight="semibold"
+        transition=".15s ease"
+        color={useColorModeValue("gray.500", "gray.400")}
+        _hover={{
+          color: useColorModeValue("gray.700", "gray.200"),
+        }}
+      >
+        {icon && (
           <Icon
-            as={RiFlashlightFill}
-            h={8}
-            w={8}
-            cursor="pointer"
-            color={useColorModeValue("gray.500", "gray.400")}
-            _hover={{
-              color: useColorModeValue("gray.700", "gray.200"),
+            mx="auto"
+            boxSize="6"
+            _groupHover={{
+              color: color,
             }}
+            as={icon}
           />
-        </Flex>
-        <Flex px="2" align="center">
-          <Divider />
-        </Flex>
+        )}
+      </Flex>
+    );
+  };
+  return (
+    <Box
+      as="nav"
+      pos="fixed"
+      top="0"
+      left="0"
+      pb="25px"
+      zIndex="1"
+      h="full"
+      overflowX="hidden"
+      overflowY="auto"
+      bg={useColorModeValue("gray.50", "gray.700")}
+    >
+      <VStack h="full" w="full" alignItems="flex-start" justify="space-between">
+        <Box w="full">
+          <Flex px="2" py="5" align="center">
+            <Icon
+              as={RiFlashlightFill}
+              h={8}
+              w={8}
+              cursor="pointer"
+              color={useColorModeValue("gray.500", "gray.400")}
+              _hover={{
+                color: useColorModeValue("gray.700", "gray.200"),
+              }}
+              onClick={() => {
+                navigate("/");
+              }}
+            />
+          </Flex>
+          <Flex px="2" align="center">
+            <Divider />
+          </Flex>
+
+          <Flex
+            direction="column"
+            as="nav"
+            fontSize="md"
+            color="gray.600"
+            aria-label="Main Navigation"
+          >
+            <NavItem icon={FaTelegramPlane} url="https://t.me/nabilaba" />
+            <NavItem icon={GoMarkGithub} url="https://github.com/nabilaba" />
+            <NavItem
+              icon={FaLinkedin}
+              url="https://www.linkedin.com/in/nabilaba/"
+            />
+            <NavItem icon={IoLogoWhatsapp} url="https://wa.me/6283146542084" />
+            <NavItem
+              icon={TiSocialInstagram}
+              url="https://instagram.com/nabil_aba"
+            />
+          </Flex>
+        </Box>
 
         <Flex
           direction="column"
-          as="nav"
-          fontSize="md"
-          color="gray.600"
-          aria-label="Main Navigation"
+          p="2"
+          mt={10}
+          justify="center"
+          alignItems="center"
         >
-          <NavItem icon={FaTelegramPlane} url="https://t.me/nabilaba" />
-          <NavItem icon={GoMarkGithub} url="https://github.com/nabilaba" />
-          <NavItem icon={FaLinkedin} url="https://www.linkedin.com/in/nabilaba/" />
-          <NavItem icon={IoLogoWhatsapp} url="https://wa.me/6283146542084" />
-          <NavItem icon={TiSocialInstagram} url="https://instagram.com/nabil_aba" />
+          <Avatar
+            size={"sm"}
+            name="Nabil"
+            src="https://avatars.githubusercontent.com/u/45154878?v=4"
+            filter="grayscale(100%)"
+            cursor={"pointer"}
+            _hover={{
+              filter: "grayscale(0%)",
+            }}
+          />
+          <IconButton
+            mt="4"
+            size={"sm"}
+            onClick={props.toggleColorMode}
+            variant="ghost"
+            icon={props.colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          ></IconButton>
         </Flex>
-      </Box>
-
-      <Flex
-        direction="column"
-        p="2"
-        mt={10}
-        justify="center"
-        alignItems="center"
-      >
-        <Avatar
-          size={"sm"}
-          name="Nabil"
-          src="https://avatars.githubusercontent.com/u/45154878?v=4"
-          filter="grayscale(100%)"
-          cursor={"pointer"}
-          _hover={{
-            filter: "grayscale(0%)",
-          }}
-        />
-        <IconButton
-          mt="4"
-          size={"sm"}
-          onClick={props.toggleColorMode}
-          variant="ghost"
-          icon={props.colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-        ></IconButton>
-      </Flex>
-    </VStack>
-  </Box>
-);
-
-const NavItem = (props) => {
-  const color = useColorModeValue("gray.600", "gray.300");
-
-  const { icon, url } = props;
-  return (
-    <Flex
-      as={Link}
-      href={url}
-      target="_blank"
-      align="center"
-      px="2"
-      py="4"
-      cursor="pointer"
-      role="group"
-      fontWeight="semibold"
-      transition=".15s ease"
-      color={useColorModeValue("gray.500", "gray.400")}
-      _hover={{
-        color: useColorModeValue("gray.700", "gray.200"),
-      }}
-    >
-      {icon && (
-        <Icon
-          mx="auto"
-          boxSize="6"
-          _groupHover={{
-            color: color,
-          }}
-          as={icon}
-        />
-      )}
-    </Flex>
+      </VStack>
+    </Box>
   );
 };
 
