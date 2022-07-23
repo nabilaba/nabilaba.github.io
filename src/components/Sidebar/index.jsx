f6c1f7e4720cdf8733b8d13a3785296d0d315bf9
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-23 12:35:59 +0700

    update item in sidebarcontent

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -6,11 +6,15 @@ import {
   useColorModeValue,
   VStack,
   IconButton,
+  Divider,
 } from "@chakra-ui/react";
-import { AiOutlineTeam, AiOutlineHome } from "react-icons/ai";
-import { BsFolder2, BsCalendarCheck } from "react-icons/bs";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { RiFlashlightFill } from "react-icons/ri";
+import { FaTelegramPlane, FaLinkedin } from "react-icons/fa";
+import { GoMarkGithub } from "react-icons/go";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { TiSocialInstagram } from "react-icons/ti";
+import { SiDiscord } from "react-icons/si";
 
 const SidebarContent = (props) => (
   <Box
@@ -28,8 +32,21 @@ const SidebarContent = (props) => (
     <VStack h="full" w="full" alignItems="flex-start" justify="space-between">
       <Box w="full">
         <Flex px="2" py="5" align="center">
-          <Icon as={RiFlashlightFill} h={8} w={8} />
+          <Icon
+            as={RiFlashlightFill}
+            h={8}
+            w={8}
+            cursor="pointer"
+            color={useColorModeValue("gray.500", "gray.400")}
+            _hover={{
+              color: useColorModeValue("gray.700", "gray.200"),
+            }}
+          />
         </Flex>
+        <Flex px="2" align="center">
+          <Divider />
+        </Flex>
+
         <Flex
           direction="column"
           as="nav"
@@ -37,10 +54,12 @@ const SidebarContent = (props) => (
           color="gray.600"
           aria-label="Main Navigation"
         >
-          <NavItem icon={AiOutlineHome} />
-          <NavItem icon={AiOutlineTeam} />
-          <NavItem icon={BsFolder2} />
-          <NavItem icon={BsCalendarCheck} />
+          <NavItem icon={FaTelegramPlane} />
+          <NavItem icon={GoMarkGithub} />
+          <NavItem icon={FaLinkedin} />
+          <NavItem icon={IoLogoWhatsapp} />
+          <NavItem icon={TiSocialInstagram} />
+          <NavItem icon={SiDiscord} />
         </Flex>
       </Box>
 
@@ -86,16 +105,15 @@ const NavItem = (props) => {
       role="group"
       fontWeight="semibold"
       transition=".15s ease"
-      color={useColorModeValue("inherit", "gray.400")}
+      color={useColorModeValue("gray.500", "gray.400")}
       _hover={{
-        bg: useColorModeValue("gray.100", "gray.900"),
-        color: useColorModeValue("gray.900", "gray.200"),
+        color: useColorModeValue("gray.700", "gray.200"),
       }}
     >
       {icon && (
         <Icon
-          mx="2"
-          boxSize="4"
+          mx="auto"
+          boxSize="6"
           _groupHover={{
             color: color,
           }}
