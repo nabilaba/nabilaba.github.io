a27cc75c72723222a7e932d53877f0eeb44a8064
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-23 23:03:05 +0700

    added react-typed for animation typing in Homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
-import { Heading, Stack } from "@chakra-ui/react";
+import { Heading, Stack, Text } from "@chakra-ui/react";
+import Typed from "react-typed";
 
 const Home = () => (
   <Stack
@@ -7,7 +8,23 @@ const Home = () => (
     justifyContent="center"
     h="full"
   >
-    <Heading fontWeight="semibold">Hi! I'm Nabil</Heading>
+    <Stack textAlign="center">
+      <Heading fontWeight="semibold">Hi! I'm Nabil</Heading>
+      <Heading fontWeight="normal" color="gray.500" fontSize="xl">
+        <Typed
+          strings={[
+            "I'm a Frontend Engineering.",
+            "Still learning for Backend Engineering.",
+            "Want to be Fullstack Engineering.",
+          ]}
+          typeSpeed={50}
+          backSpeed={50}
+          backDelay={1000}
+          loop
+          smartBackspace
+        />
+      </Heading>
+    </Stack>
   </Stack>
 );
 
