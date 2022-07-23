77802627936be6d6eace3641247a1c739ad9c3b2
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-23 21:49:09 +0700

    changing container Main to flex

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,17 +1,20 @@
-import {
-  Box,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
 import Home from "../../pages/Home";
 import HeaderContent from "../Header";
 
 const MainContent = () => (
-  <Box ml={{ base: 12, md: 12 }} transition=".3s ease">
+  <Flex
+    ml={{ base: 12, md: 12 }}
+    transition=".3s ease"
+    direction="column"
+    h="100vh"
+    pb="25px"
+  >
     <HeaderContent />
-    <Box as="main" bgColor={useColorModeValue("gray.100", "gray.800")}>
-        <Home />
+    <Box as="main" bgColor={useColorModeValue("gray.100", "gray.800")} flex={1}>
+      <Home />
     </Box>
-  </Box>
+  </Flex>
 );
 
 export default MainContent;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,8 +6,6 @@ const Home = () => (
     alignItems="center"
     justifyContent="center"
     h="full"
-    minH="96vh"
-    pb="4"
   >
     <Heading fontWeight="semibold">Hi! I'm Nabil</Heading>
   </Stack>
