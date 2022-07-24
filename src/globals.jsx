0e4e410ba1833475070832ac2f5d3e3f1a7ca588
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-24 09:43:22 +0700

    added custom scrollbar webkit

---
+++ src/globals.jsx
@@ -0,0 +1,35 @@
+import { mode } from "@chakra-ui/theme-tools";
+
+export const styles = {
+  global: (props) => ({
+    "::-webkit-scrollbar": {
+      width: "14px",
+      height: "14px",
+    },
+    "::-webkit-scrollbar-thumb": {
+      background: mode("gray.400", "gray.700")(props),
+      borderLeft: mode("1px solid gray", "1px solid gray")(props),
+      borderRadius: "0px",
+    },
+    "::-webkit-scrollbar-thumb:hover": {
+      background: mode("gray.400", "gray.700")(props),
+    },
+    "::-webkit-scrollbar-thumb:active": {
+      background: mode("gray.400", "gray.700")(props),
+    },
+    "::-webkit-scrollbar-track": {
+      background: mode("#e6e6e6", "gray.600")(props),
+      borderLeft: mode("1px solid gray", "1px solid gray")(props),
+      borderRadius: "0px",
+    },
+    "::-webkit-scrollbar-track:hover": {
+      background: mode("#e6e6e6", "gray.600")(props),
+    },
+    "::-webkit-scrollbar-track:active": {
+      background: mode("#e6e6e6", "gray.600")(props),
+    },
+    "::-webkit-scrollbar-corner": {
+      background: "transparent",
+    },
+  }),
+};
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { styles } from "./globals";
 
 const theme = extendTheme({
+  styles,
   config: {
     useSystemColorMode: false,
     initialColorMode: "dark",
