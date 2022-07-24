97618c64576be64248b574b406695b5b315463b1
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-24 11:42:02 +0700

    update font to ubuntu mono

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,15 @@ import App from "./App";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { styles } from "./globals";
+import "@fontsource/ubuntu-mono";
 
 const theme = extendTheme({
   styles,
+  fonts: {
+    body: "Ubuntu Mono, system-ui, sans-serif",
+    heading: "Ubuntu Mono, system-ui, sans-serif",
+    monospace: "Menlo, monospace",
+  },
   config: {
     useSystemColorMode: false,
     initialColorMode: "dark",
