13c0409f953c5f553bf32bfcd648c4aeebed4890
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-24 13:42:42 +0700

    updating preview to public path

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -3,9 +3,7 @@ import { Helmet } from "react-helmet";
 const Title = ({ title, subtitle }) => (
   <Helmet>
     <title>{title}</title>
-    <meta name="description" content={subtitle} />
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={subtitle} />
+    <meta name="title" property="og:title" content={title} />
   </Helmet>
 );
 
