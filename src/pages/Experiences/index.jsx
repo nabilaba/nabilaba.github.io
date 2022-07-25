189824d4941e4413bc27e439d8258d65b6398e8b
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-25 13:37:18 +0700

    added data to pages experience

--- src/pages/Experiences/index.jsx
+++ src/pages/Experiences/index.jsx
@@ -1,15 +1,33 @@
-import { Heading, Stack } from "@chakra-ui/react";
+import {
+  Heading,
+  ListItem,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import PagesItem from "../../components/PagesItem";
 import Title from "../../components/Title";
 
 const Experiences = () => (
-  <Stack
-    direction={{ base: "column", sm: "row" }}
-    alignItems="center"
-    justifyContent="center"
-    h="full"
-  >
+  <Stack spacing={5} h="full">
     <Title title={`experiences - Nabil Aba`} />
-    <Heading>Experiences</Heading>
+    <Heading color="blue.500" fontSize="xl">
+      My experiences
+    </Heading>
+    <Stack color={useColorModeValue("gray.600", "gray.400")}>
+      <PagesItem>
+        <ListItem>
+          Cyber Security Analyst at Kominfo from July 2022 until now
+        </ListItem>
+        <ListItem>
+          Frontend Engineering Student at Ruangguru from February 2022 to July
+          2022
+        </ListItem>
+        <ListItem>
+          Features Maker and User Experience Designer at AncientOS from May 2022
+        </ListItem>
+      </PagesItem>
+    </Stack>
   </Stack>
 );
 
